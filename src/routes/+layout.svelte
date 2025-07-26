d4cab7acacd80b6f560f6913b983e6b7ee5e9329
Angus White <me@anguswhite.dev>
2025-07-26 15:37:50 +1000

    Micro-project post

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,26 +6,15 @@
 </script>
 
 <title>anguswhite.dev</title>
-<nav class="mx-auto flex place-items-center justify-between p-6 lg:px-8" aria-label="Global">
-	<div class="flex gap-x-12">
-		<a href="/" class="-m-1.5 p-1.5">
-			<span class="sr-only">Your Company</span>
-			<img
-				class="h-8 w-auto"
-				src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-				alt=""
-			/>
-		</a>
-
-		<a href="/" class="text-sm/6 font-semibold text-gray-900">About</a>
-		<a href="/posts" class="text-sm/6 font-semibold text-gray-900">Posts</a>
-	</div>
-</nav>
-<main class="prose prose-stone prose-lg prose-headings:font-[Bentham] mx-auto">
-	{@render children()}
-</main>
-<footer>
-	<div class="mx-auto w-full max-w-screen-xl">
-		<a href="mailto:me@anguswhite.dev" class="text-sm/6"> Email Me </a>
-	</div>
-</footer>
+<div class="flex flex-col h-screen justify-between">
+  <nav class="flex gap-x-8 px-6 py-6" aria-label="Navbar">
+    <a href="/" class="text-sm/6 font-semibold text-gray-900">About</a>
+    <a href="/posts" class="text-sm/6 font-semibold text-gray-900">Posts</a>
+  </nav>
+  <main class="flex-grow prose prose-stone prose-lg prose-headings:font-[Bentham] mx-auto p-4">
+    {@render children()}
+  </main>
+  <footer class="mx-auto flex gap-x-6 place-content-center px-3 py-4 static bottom-0 w-full" aria-label="Footer">
+    <a href="mailto:me@anguswhite.dev" class="text-sm/6 font-semibold text-gray-900">Email Me</a>
+  </footer>
+</div>
